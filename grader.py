311ea5f4e4028e1225e14e14cf398e54f54bb6ed
Vansh Rajput <67141792+vanshrajput3110@users.noreply.github.com>
2026-04-09 10:21:14 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,10 +1,8 @@
 def grade_easy(pred, actual):
     return 0.9 if pred == actual else 0.2
 
-
 def grade_medium(pred, actual):
     return 0.85 if pred == actual else 0.3
-
 
 def grade_hard(pred, expected_keywords):
     return 0.6
